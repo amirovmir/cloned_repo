3b63ec6c6588dea03af195938e655f4b5e40036e
amirovmir <78919537+amirovmir@users.noreply.github.com>
2022-07-19 21:26:52 +0300

    Delete 2022/hello_on_kos directory

--- 2022/hello_on_kos/common/common.h
+++
@@ -1,9 +0,0 @@
-/* © 2021 AO Kaspersky Lab */
-#ifndef HELLO_ON_KOS_COMMON_H
-#define HELLO_ON_KOS_COMMON_H
-
-#define HELLO_PARTITION 1U
-
-int MountFileSystem(unsigned partitionId);
-
-#endif
--- 2022/hello_on_kos/common/file_names.h
+++
@@ -1,7 +0,0 @@
-/* © 2021 AO Kaspersky Lab */
-#ifndef SEPARATE_STORAGE_FILE_NAMES_H
-#define SEPARATE_STORAGE_FILE_NAMES_H
-
-const char *HelloListFileName = "/c/hello.txt";
-
-#endif
